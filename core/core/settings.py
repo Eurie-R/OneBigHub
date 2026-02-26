582fd3f331072b39dbc6c7db70ec788b6bd610df
Danielle Sulay <lucia.sulay@student.ateneo.edu>
2026-02-26 22:36:05 +0800

    Initial landing page, header, hero section, static files

--- core/core/settings.py
+++ core/core/settings.py
@@ -195,7 +195,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / "core" / "static",
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
