aa5d91d0039165be8290ba3c45b84b04f6cf60f1
Luis Felipe <luisfelipecodes@gmail.com>
2026-03-05 13:11:46 +0800

    Added URL route for proposal form

--- core/proposals/urls.py
+++ core/proposals/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    # Routes will be added in Iteration 1 & 2
+    path('create/', views.create_proposal, name='create_proposal'),
 ]
