00c37167f5460d563fbf3f070016e850c58cdcc8
Luis Felipe <luisfelipecodes@gmail.com>
2026-03-05 14:52:47 +0800

    Updated initial create proposal html and debugged proposals app name

--- core/proposals/urls.py
+++ core/proposals/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'accounts'
+app_name = 'proposals'
 
 urlpatterns = [
     path('create/', views.create_proposal, name='create_proposal'),
