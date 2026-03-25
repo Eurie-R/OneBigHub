30cdf920c707adaff8e9cdb345c089c0a157aa3e
fckjava <mattjoaquim37@gmail.com>
2026-03-24 22:07:29 -0700

    Venue type distinction and classification

--- core/venues/views.py
+++ core/venues/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Venue, Reservation
 
-# Create your views here.
+def venue_list(request):
+    type_one_venues = Venue.objects.filter(venue_type = "1")
+    type_two_venues = Venue.objects.filter(venue_type = "2")
+    
