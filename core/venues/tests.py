2ed2e845205b6e4a76eda173cd64491b3da20716
fckjava <mattjoaquim37@gmail.com>
2026-03-26 10:53:35 -0700

    Tests for venue types and character limits

--- core/venues/tests.py
+++ core/venues/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Reservation, Venue
 
-# Create your tests here.
+class VenueTestCase(TestCase):
+    def setUp(self):
+        self.venue1 = Venue.objects.create(name="Colayco Pavillion", venue_type="1", description="Colayco Pavillion is a roofed but semi-outdoor venue with a capacity of 50.")
+        self.venue2 = Venue.objects.create(name="Blue Eagle Gym", venue_type="2", description="Ateneo's new and improved gymnasium that holds 7,000.")
+
+#Check if venues can be correctly identified as type 1 or type 2
+    def test_organize_by_type(self):
+        self.assertEqual(self.venue1.venue_type, "1")
+        self.assertEqual(self.venue2.venue_type, "2")
+
+    def test_description_limit(self):
+        self.assertLess(len(self.venue1.description), 256)
+        self.assertLess(len(self.venue2.description), 256)
+
+    def test_name_limit(self):
+        self.assertLess(len(self.venue1.name), 256)
+        self.assertLess(len(self.venue2.name), 256)
