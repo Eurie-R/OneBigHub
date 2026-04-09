756f22f4da0fd9bcf003fe50fe750ddba93e2dd5
chrisxdale <chrisxdalelopez@gmail.com>
2026-04-09 17:49:48 +0800

    Updated urls.py to add new html templates
    Added the new html templates in the urls.py

--- core/feed/urls.py
+++ core/feed/urls.py
@@ -4,7 +4,13 @@ from . import views
 app_name = 'feed'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('create/', views.post_create, name='post_create'),
-    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('', views.feed_list, name='feed_list'),
+    path('create/', views.feed_create, name='feed_create'),
+    path('<int:pk>/', views.feed_detail, name='feed_detail'),
+    path('<int:pk>/edit/', views.feed_edit, name='feed_edit'),
+    path('<int:pk>/delete/', views.feed_delete, name='feed_delete'),
+    
+    path('api/', views.post_list, name='post_list'),
+    path('api/create/', views.post_create, name='post_create'),
+    path('api/<int:pk>/', views.post_detail, name='post_detail_api'),
 ]
