181067650eae753d740a524d266cb7880714a807
chrisxdale <chrisxdalelopez@gmail.com>
2026-04-09 13:30:28 +0800

    Added Post class in feed models.py
    Created class Post which refers to the feed post created by the users.

--- core/feed/models.py
+++ core/feed/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import OrganizationProfile
 
 # Create your models here.
+
+class Post(models.Model):
+    organization = models.ForeignKey(
+        OrganizationProfile,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+
+    event_start = models.DateTimeField(null=True, blank=True)
+    event_end = models.DateTimeField(null=True, blank=True)
+    location = models.CharField(max_length=255, blank=True)
