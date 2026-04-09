b8ace4b1eeceb04d1f805c19c58aeef88998f17a
chrisxdale <chrisxdalelopez@gmail.com>
2026-04-09 17:32:31 +0800

    Created forms.py under feed
    This form take in user input to be stored in the database.

---
+++ core/feed/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'body', 'image', 'event_start', 'event_end', 'location']
+        widgets = {
+            'event_start': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'event_end':   forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+ 
+    def clean(self):
+        cleaned_data = super().clean()
+        start = cleaned_data.get('event_start')
+        end = cleaned_data.get('event_end')
+ 
+        if bool(start) != bool(end):
+            raise forms.ValidationError(
+                "Both event start and event end must be provided together."
+            )
+        if start and end and end <= start:
+            raise forms.ValidationError(
+                "Event end must be after event start."
+            )
+        return cleaned_data
