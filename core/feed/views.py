e7cebc58be9eb18e81389d71a88b194ab43855ef
chrisxdale <chrisxdalelopez@gmail.com>
2026-04-09 14:01:06 +0800

    Added post_list function in views.py
    Returns all the organization post in the feed. Also allows organizations to filter which organizations to see.

--- core/feed/views.py
+++ core/feed/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+ 
+from users.permissions import IsAteneoUser, IsOrganization
+from .models import Post
+from .serializers import PostSerializer, PostCreateSerializer
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAteneoUser])
+def post_list(request):
+
+    posts = Post.objects.select_related('organization').all()
+ 
+    org_id = request.query_params.get('org')
+    if org_id:
+        posts = posts.filter(organization__id=org_id)
+ 
+    serializer = PostSerializer(posts, many=True, context={'request': request})
+    return Response(serializer.data, status=status.HTTP_200_OK)
