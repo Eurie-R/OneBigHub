f78fdab7583b32bed7761cc9536cc3df2170792c
chrisxdale <chrisxdalelopez@gmail.com>
2026-04-09 14:38:17 +0800

    Updated admin.py
    Include an Admin Class for Post

--- core/feed/admin.py
+++ core/feed/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'organization', 'title', 'event_start', 'event_end', 'created_at']
+    list_filter = ['organization', 'created_at']
+    search_fields = ['title', 'body', 'organization__org_name']
+    ordering = ['-created_at']
+    readonly_fields = ['created_at', 'updated_at']
